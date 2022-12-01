faef543b498169e094c00d1dc68d50b8a119989e
Kleverson Eller <kleverson.a16@gmail.com>
2022-12-01 04:22:01 -0300

    build: :building_construction: Configuraçoes iniciais (Tailwindcss)

---
+++ packages/web/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./src/**/*.{html,js,ts,tsx}',
+		'./index.html',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
